ff9bed34d0306da6802273120ed41bdd2e35fc3a
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@github.com/hamzasorani83/Portfolio <hamzasorani83@gmail.com>
2023-01-24 13:26:13 +0300

    real api

--- src/components/axiosInstance/axios.ts
+++ src/components/axiosInstance/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:4000/',
+  baseURL: 'https://social-media-api-8ch8.onrender.com/',
 })
 
 export default instance;
